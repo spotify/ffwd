2693707f47efc93af7b5a7d9793f424e8e66530a
John-John Tedro <udoprog@spotify.com>
2015-03-18 16:50:59 +0100

    [core] Remove java 8 Long.BYTES

--- core/src/main/java/com/spotify/ffwd/qlog/QLogManagerImpl.java
+++ core/src/main/java/com/spotify/ffwd/qlog/QLogManagerImpl.java
@@ -2,17 +2,14 @@
 /**
  * Copyright 2013-2014 Spotify AB. All rights reserved.
  *
- * The contents of this file are licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * The contents of this file are licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  **/
 package com.spotify.ffwd.qlog;
 
@@ -55,7 +52,7 @@ public class QLogManagerImpl implements QLogManager {
 
     // 'FFLG'
     private static final byte[] MAGIC = new byte[] { 0x46, 0x46, 0x4c, 0x47 };
-    private static final int HEADER_BASE_SIZE = MAGIC.length + Long.BYTES;
+    private static final int HEADER_BASE_SIZE = MAGIC.length + 8;
     private static final int BUFFER_SIZE = 4096;
     private static final int CURRENT_VERSION = 0;
 
