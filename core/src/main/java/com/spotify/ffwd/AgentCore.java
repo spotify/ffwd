ad4abf2fb1b1d59ae870037cb8b4a624fb4f3a6a
John-John Tedro <udoprog@spotify.com>
2015-05-20 16:13:54 +0200

    [build] bump version of tiny-async to 1.5.0

--- core/src/main/java/com/spotify/ffwd/AgentCore.java
+++ core/src/main/java/com/spotify/ffwd/AgentCore.java
@@ -2,17 +2,14 @@
 /**
  * Copyright 2013-2014 Spotify AB. All rights reserved.
  *
- * The contents of this file are licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * The contents of this file are licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  **/
 package com.spotify.ffwd;
 
@@ -71,8 +68,8 @@ import com.spotify.ffwd.serializer.ToStringSerializer;
 import eu.toolchain.async.AsyncCaller;
 import eu.toolchain.async.AsyncFramework;
 import eu.toolchain.async.AsyncFuture;
+import eu.toolchain.async.DirectAsyncCaller;
 import eu.toolchain.async.TinyAsync;
-import eu.toolchain.async.caller.DirectAsyncCaller;
 
 @Slf4j
 public class AgentCore {
