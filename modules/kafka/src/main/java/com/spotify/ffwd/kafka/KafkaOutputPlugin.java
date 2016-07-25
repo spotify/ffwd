spotify/ffwd

commit 8e391de074a22a580bd7074ca74aa592e86b2540

Replace Guava Optional with java.util.Optional

modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaOutputPlugin.java
@@ -17,7 +17,6 @@ package com.spotify.ffwd.kafka;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Optional;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.Provides;
@@ -35,6 +34,7 @@ import kafka.producer.ProducerConfig;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Optional;
 
 public class KafkaOutputPlugin implements OutputPlugin {
     public static final int DEFAULT_BATCH_SIZE = 1000;
@@ -43,8 +43,8 @@ public class KafkaOutputPlugin implements OutputPlugin {
     private final KafkaRouter router;
     private final KafkaPartitioner partitioner;
     private final Map<String, String> properties;
-    private final Long flushInterval;
-    private final Serializer serializer;
+    private final Optional<Long> flushInterval;
+    private final Optional<Serializer> serializer;
     private final int batchSize;
     private final boolean compression;
 
@@ -58,13 +58,14 @@ public class KafkaOutputPlugin implements OutputPlugin {
         @JsonProperty("batchSize") Integer batchSize,
         @JsonProperty("compression") Boolean compression
     ) {
-        this.router = Optional.fromNullable(router).or(KafkaRouter.Tag.supplier());
-        this.partitioner = Optional.fromNullable(partitioner).or(KafkaPartitioner.Host.supplier());
-        this.flushInterval = Optional.fromNullable(flushInterval).orNull();
-        this.properties = Optional.fromNullable(properties).or(new HashMap<String, String>());
-        this.serializer = Optional.fromNullable(serializer).orNull();
-        this.batchSize = Optional.fromNullable(batchSize).or(DEFAULT_BATCH_SIZE);
-        this.compression = Optional.fromNullable(compression).or(DEFAULT_COMPRESSION);
+        this.router = Optional.ofNullable(router).orElseGet(KafkaRouter.Tag.supplier());
+        this.partitioner = Optional.ofNullable(partitioner)
+                                   .orElseGet(KafkaPartitioner.Host.supplier());
+        this.flushInterval = Optional.ofNullable(flushInterval);
+        this.properties = Optional.ofNullable(properties).orElseGet(() -> new HashMap<>());
+        this.serializer = Optional.ofNullable(serializer);
+        this.batchSize = Optional.ofNullable(batchSize).orElse(DEFAULT_BATCH_SIZE);
+        this.compression = Optional.ofNullable(compression).orElse(DEFAULT_COMPRESSION);
     }
 
     @Override
@@ -92,16 +93,16 @@ public class KafkaOutputPlugin implements OutputPlugin {
                 bind(KafkaRouter.class).toInstance(router);
                 bind(KafkaPartitioner.class).toInstance(partitioner);
 
-                if (serializer == null) {
+                if (serializer.isPresent()) {
                     // bind to default implementation, provided by core.
                     bind(Serializer.class).to(Key.get(Serializer.class, Names.named("default")));
                 } else {
-                    bind(Serializer.class).toInstance(serializer);
+                    bind(Serializer.class).toInstance(serializer.get());
                 }
 
-                if (flushInterval != null) {
+                if (flushInterval.isPresent()) {
                     bind(BatchedPluginSink.class).toInstance(new KafkaPluginSink(batchSize));
-                    bind(key).toInstance(new FlushingPluginSink(flushInterval));
+                    bind(key).toInstance(new FlushingPluginSink(flushInterval.get()));
                 } else {
                     bind(key).toInstance(new KafkaPluginSink(batchSize));
                 }

api/src/main/java/com/spotify/ffwd/filter/TrueFilter.java
@@ -19,12 +19,12 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.google.common.base.Supplier;
 import com.spotify.ffwd.model.Event;
 import com.spotify.ffwd.model.Metric;
 import lombok.Data;
 
 import java.io.IOException;
+import java.util.function.Supplier;
 
 @Data
 public class TrueFilter implements Filter {

api/src/main/java/com/spotify/ffwd/protocol/ProtocolFactory.java
@@ -17,10 +17,10 @@ package com.spotify.ffwd.protocol;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Supplier;
 import lombok.Data;
 
 import java.net.InetSocketAddress;
+import java.util.function.Supplier;
 
 /**
  * Data type suitable for building using a @JsonCreator block.

api/src/main/java/com/spotify/ffwd/protocol/RetryPolicy.java
@@ -20,8 +20,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.google.common.base.Optional;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
@@ -51,7 +51,7 @@ public interface RetryPolicy {
 
         @JsonCreator
         public Constant(@JsonProperty("value") Long value) {
-            this.value = Optional.fromNullable(value).or(DEFAULT_VALUE);
+            this.value = Optional.ofNullable(value).orElse(DEFAULT_VALUE);
         }
 
         @Override
@@ -75,8 +75,8 @@ public interface RetryPolicy {
 
         @JsonCreator
         public Exponential(@JsonProperty("initial") Long initial, @JsonProperty("max") Long max) {
-            this.initial = Optional.fromNullable(initial).or(DEFAULT_INITIAL);
-            this.max = Optional.fromNullable(max).or(DEFAULT_MAX);
+            this.initial = Optional.ofNullable(initial).orElse(DEFAULT_INITIAL);
+            this.max = Optional.ofNullable(max).orElse(DEFAULT_MAX);
             this.maxAttempt =
                 new Double(Math.floor(Math.log(this.max / this.initial) / Math.log(2))).intValue();
         }
@@ -108,8 +108,8 @@ public interface RetryPolicy {
 
         @JsonCreator
         public Linear(@JsonProperty("value") Long value, @JsonProperty("max") Long max) {
-            this.value = Optional.fromNullable(value).or(DEFAULT_VALUE);
-            this.max = Optional.fromNullable(max).or(DEFAULT_MAX);
+            this.value = Optional.ofNullable(value).orElse(DEFAULT_VALUE);
+            this.max = Optional.ofNullable(max).orElse(DEFAULT_MAX);
             this.maxAttempt = (int) ((this.max / this.value) - 1);
         }
 

core/src/main/java/com/spotify/ffwd/AgentConfig.java
@@ -17,8 +17,6 @@ package com.spotify.ffwd;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Optional;
-import com.google.common.base.Supplier;
 import com.google.common.collect.Maps;
 import com.spotify.ffwd.input.InputManagerModule;
 import com.spotify.ffwd.output.OutputManagerModule;
@@ -30,6 +28,7 @@ import java.net.UnknownHostException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
 
 @Data
 public class AgentConfig {
@@ -65,27 +64,18 @@ public class AgentConfig {
         @JsonProperty("workerThreads") Integer workerThreads, @JsonProperty("ttl") Long ttl,
         @JsonProperty("qlog") String qlog
     ) {
-        this.debug = Optional.fromNullable(debug);
-        this.host = Optional.fromNullable(host).or(this.defaultHostSupplier());
-        this.tags = Optional.fromNullable(tags).or(DEFAULT_TAGS);
-        this.input = Optional.fromNullable(input).or(InputManagerModule.supplyDefault());
-        this.output = Optional.fromNullable(output).or(OutputManagerModule.supplyDefault());
-        this.asyncThreads = Optional.fromNullable(asyncThreads).or(DEFAULT_ASYNC_THREADS);
+        this.debug = Optional.ofNullable(debug);
+        this.host = Optional.ofNullable(host).orElseGet(this::buildDefaultHost);
+        this.tags = Optional.ofNullable(tags).orElse(DEFAULT_TAGS);
+        this.input = Optional.ofNullable(input).orElseGet(InputManagerModule.supplyDefault());
+        this.output = Optional.ofNullable(output).orElseGet(OutputManagerModule.supplyDefault());
+        this.asyncThreads = Optional.ofNullable(asyncThreads).orElse(DEFAULT_ASYNC_THREADS);
         this.schedulerThreads =
-            Optional.fromNullable(schedulerThreads).or(DEFAULT_SCHEDULER_THREADS);
-        this.bossThreads = Optional.fromNullable(bossThreads).or(DEFAULT_BOSS_THREADS);
-        this.workerThreads = Optional.fromNullable(workerThreads).or(DEFAULT_WORKER_THREADS);
-        this.ttl = Optional.fromNullable(ttl).or(0L);
-        this.qlog = Paths.get(Optional.fromNullable(qlog).or(DEFAULT_QLOG));
-    }
-
-    private Supplier<String> defaultHostSupplier() {
-        return new Supplier<String>() {
-            @Override
-            public String get() {
-                return buildDefaultHost();
-            }
-        };
+            Optional.ofNullable(schedulerThreads).orElse(DEFAULT_SCHEDULER_THREADS);
+        this.bossThreads = Optional.ofNullable(bossThreads).orElse(DEFAULT_BOSS_THREADS);
+        this.workerThreads = Optional.ofNullable(workerThreads).orElse(DEFAULT_WORKER_THREADS);
+        this.ttl = Optional.ofNullable(ttl).orElse(0L);
+        this.qlog = Paths.get(Optional.ofNullable(qlog).orElse(DEFAULT_QLOG));
     }
 
     private String buildDefaultHost() {
@@ -105,8 +95,8 @@ public class AgentConfig {
 
         @JsonCreator
         public Debug(@JsonProperty("host") String host, @JsonProperty("port") Integer port) {
-            this.localAddress = buildLocalAddress(Optional.fromNullable(host).or(DEFAULT_HOST),
-                Optional.fromNullable(port).or(DEFAULT_PORT));
+            this.localAddress = buildLocalAddress(Optional.ofNullable(host).orElse(DEFAULT_HOST),
+                Optional.ofNullable(port).orElse(DEFAULT_PORT));
         }
 
         private InetSocketAddress buildLocalAddress(String host, Integer port) {

core/src/main/java/com/spotify/ffwd/generated/GeneratedInputPlugin.java
@@ -17,19 +17,20 @@ package com.spotify.ffwd.generated;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Optional;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.PrivateModule;
 import com.spotify.ffwd.input.InputPlugin;
 import com.spotify.ffwd.input.PluginSource;
 
+import java.util.Optional;
+
 public class GeneratedInputPlugin implements InputPlugin {
     private final boolean sameHost;
 
     @JsonCreator
     public GeneratedInputPlugin(@JsonProperty("sameHost") Boolean sameHost) {
-        this.sameHost = Optional.fromNullable(sameHost).or(false);
+        this.sameHost = Optional.ofNullable(sameHost).orElse(false);
     }
 
     @Override

core/src/main/java/com/spotify/ffwd/input/InputManagerModule.java
@@ -17,8 +17,6 @@ package com.spotify.ffwd.input;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Optional;
-import com.google.common.base.Supplier;
 import com.google.common.collect.Lists;
 import com.google.inject.Key;
 import com.google.inject.Module;
@@ -36,6 +34,8 @@ import io.netty.channel.ChannelInboundHandler;
 
 import java.util.List;
 import java.util.Set;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class InputManagerModule {
     private static final List<InputPlugin> DEFAULT_PLUGINS = Lists.newArrayList();
@@ -47,8 +47,8 @@ public class InputManagerModule {
     public InputManagerModule(
         @JsonProperty("plugins") List<InputPlugin> plugins, @JsonProperty("filter") Filter filter
     ) {
-        this.plugins = Optional.fromNullable(plugins).or(DEFAULT_PLUGINS);
-        this.filter = Optional.fromNullable(filter).or(new TrueFilter());
+        this.plugins = Optional.ofNullable(plugins).orElse(DEFAULT_PLUGINS);
+        this.filter = Optional.ofNullable(filter).orElseGet(() -> new TrueFilter());
     }
 
     public Module module() {

core/src/main/java/com/spotify/ffwd/noop/NoopOutputPlugin.java
@@ -17,7 +17,6 @@ package com.spotify.ffwd.noop;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Optional;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
@@ -27,6 +26,7 @@ import com.spotify.ffwd.output.OutputPlugin;
 import com.spotify.ffwd.output.OutputPluginModule;
 import com.spotify.ffwd.output.PluginSink;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class NoopOutputPlugin implements OutputPlugin {
@@ -37,7 +37,7 @@ public class NoopOutputPlugin implements OutputPlugin {
 
     @JsonCreator
     public NoopOutputPlugin(@JsonProperty("flushInterval") Long flushInterval) {
-        this.flushInterval = Optional.fromNullable(flushInterval).or(DEFAULT_FLUSH_INTERVAL);
+        this.flushInterval = Optional.ofNullable(flushInterval).orElse(DEFAULT_FLUSH_INTERVAL);
     }
 
     @Override

commit 86542164a2ffe0d20ab89494dac18ef98dabb06f

Fix serializer configuration in KafkaOutputPlugin
Broken by 8e391de074a22a580bd7074ca74aa592e86b2540

modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaOutputPlugin.java
@@ -94,10 +94,10 @@ public class KafkaOutputPlugin implements OutputPlugin {
                 bind(KafkaPartitioner.class).toInstance(partitioner);
 
                 if (serializer.isPresent()) {
+                    bind(Serializer.class).toInstance(serializer.get());
+                } else {
                     // bind to default implementation, provided by core.
                     bind(Serializer.class).to(Key.get(Serializer.class, Names.named("default")));
-                } else {
-                    bind(Serializer.class).toInstance(serializer.get());
                 }
 
                 if (flushInterval.isPresent()) {