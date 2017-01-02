2c463eb11b390d693b0b02b2179140267ca15d07
John-John Tedro <udoprog@spotify.com>
2017-01-02 10:26:16 +0100

    [modules/kafka] fix style issue

--- modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaPluginSink.java
+++ modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaPluginSink.java
@@ -37,7 +37,6 @@ import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
