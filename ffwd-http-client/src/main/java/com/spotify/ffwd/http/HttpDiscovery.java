1efcc0abaa2d07804e1198f648efd37d85ae12b5
Kateryna Nezdolii <nezdolik@spotify.com>
2017-12-12 17:03:53 +0100

    No search domain for localhost or hostname ending with '.'

--- ffwd-http-client/src/main/java/com/spotify/ffwd/http/HttpDiscovery.java
+++ ffwd-http-client/src/main/java/com/spotify/ffwd/http/HttpDiscovery.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013-2017 Spotify AB. All rights reserved.
- *
+ * <p>
  * The contents of this file are licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -96,6 +96,9 @@ public interface HttpDiscovery {
         }
 
         public HostAndPort withOptionalSearchDomain(final Optional<String> searchDomain) {
+            if (host.equals("localhost") || host.endsWith(".")) {
+                return this;
+            }
             return searchDomain.map(s -> new HostAndPort(host + "." + s, port)).orElse(this);
         }
     }
